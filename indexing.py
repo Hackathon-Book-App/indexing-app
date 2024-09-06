28ab855419dfa45b798c39edfe842fd0fe7dc9fd
Raouldepaste <raoulivan81@gmail.com>
2024-09-06 16:34:13 +0300

    rolled back to main

--- indexing.py
+++ indexing.py
@@ -13,39 +13,8 @@ load_dotenv(".venv/.env")
 
 from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
-
-
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from chromadb.utils.batch_utils import create_batches
-import uuid
-import chromadb
 
-client = chromadb.PersistentClient(path="../EmbeddedBooks",settings=chromadb.Settings(allow_reset=True))
-client.reset()
-
-# import chromadb.utils.embedding_functions as embedding_functions
-# openai_ef = embedding_functions.OpenAIEmbeddingFunction(
-#                 api_key="OPENAI_API_KEY",
-#                 model_name="text-embedding-ada-002"
-#             )
-
-colection=client.get_or_create_collection("bookEmbeddings",embedding_function=OpenAIEmbeddings)
-
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=300)
 splits = text_splitter.split_documents(docs)
-
-
-for batch in create_batches(
-    api=client,
-    ids=[str(uuid.uuid4()) for _ in range(len(splits))],
-    metadatas=[doc.metadata for doc in splits],
-    documents=[doc.page_content for doc in splits],
-):
-    
-    colection.add(ids=batch[0],
-                   documents=batch[3],
-                   embeddings=batch[1],
-                   metadatas=batch[2])
-
-    vectortore= Chroma(client=client, collection_name=colection.name, embedding_function=OpenAIEmbeddings)
-
+vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings(), persist_directory="..\\embeddedBooksDB")
